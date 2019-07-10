4116a566133ab025ed0dcaf72016211312701f26
Mario Perrotta <mario.perrotta@unimi.it>
2019-07-10 12:22:01 +0200

    Updated

--- lib/Storage/OneDrive.php
+++ lib/Storage/OneDrive.php
@@ -177,7 +177,7 @@ class OneDrive extends \OC\Files\Storage\Flysystem {
 
 		$externalConfig = $mapper->findByKey($backendID, $key);
 
-		$mapper->updateTokenByMountIdAndKey($backendID, $key, $newToken);
+		$mapper->updateTokenByMountIdAndKey($backendID, $key, json_encode($newToken));
 
 		$externalConfig = $mapper->findByKey($backendID, $key);
 
