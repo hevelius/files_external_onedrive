129af4c5134488abd02b4059626c4cec7364a810
Mario Perrotta <mario.perrotta@unimi.it>
2019-07-11 09:35:40 +0200

    Update

--- lib/Storage/OneDrive.php
+++ lib/Storage/OneDrive.php
@@ -94,7 +94,7 @@ class OneDrive extends \OC\Files\Storage\Flysystem {
 				}
 			}
 
-			$user = OCP\User::getUser();
+			$user = OC\User\Session::getUser();
 
 			$this->accessToken = $this->token->access_token;
 
