37b342e035fdd0f00199d6c0de677cbc08fdf4a7
Mario Perrotta <mario.perrotta@unimi.it>
2020-06-24 15:51:01 +0200

    updated interval time for job

--- lib/BackgroundJob/RefreshToken.php
+++ lib/BackgroundJob/RefreshToken.php
@@ -31,7 +31,7 @@ class RefreshToken extends TimedJob {
 	private $appName = 'files_external_onedrive';
 
 	public function __construct() {
-		$this->setInterval(60);
+		$this->setInterval(1);
 	}
 
 	/**
